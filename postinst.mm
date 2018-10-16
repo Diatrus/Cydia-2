7fa5a0b401e22571e7de3713765a10640a36902d
Sam Bingner <sam@bingner.com>
2018-10-15 17:30:57 -1000

    I don't care about stash in iOS11

--- postinst.mm
+++ postinst.mm
@@ -226,7 +226,7 @@ int main(int argc, const char *argv[]) {
 
     bool restart(false);
 
-    if (kCFCoreFoundationVersionNumber >= 1000) {
+    if (kCFCoreFoundationVersionNumber >= 1000 && kCFCoreFoundationVersionNumber < 1349.56) {
         if (!FixProtections())
             return 1;
         switch (MoveStash()) {
@@ -289,9 +289,11 @@ int main(int argc, const char *argv[]) {
         chown(NewLibrary_ Cytore_, 501, 501);
     }
 
-    FixPermissions();
+    if (kCFCoreFoundationVersionNumber < 1349.56) {
+        FixPermissions();
 
-    restart |= FixApplications();
+        restart |= FixApplications();
+    }
 
     if (restart)
         Finish("restart");
